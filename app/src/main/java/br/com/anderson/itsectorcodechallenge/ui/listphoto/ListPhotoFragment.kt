eab3f1186e2d0c1de380d57738ee6e0a1b0b9ee2
Anderson Soares <anderson.r.soares@gmail.com>
2020-11-07 17:35:58 -0300

    inicio

--- app/src/main/java/br/com/anderson/itsectorcodechallenge/ui/listphoto/ListPhotoFragment.kt
+++ app/src/main/java/br/com/anderson/itsectorcodechallenge/ui/listphoto/ListPhotoFragment.kt
@@ -2,13 +2,134 @@ package br.com.anderson.itsectorcodechallenge.ui.listphoto
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.anderson.itsectorcodechallenge.R
+import br.com.anderson.itsectorcodechallenge.databinding.FragmentListPhotoBinding
 import br.com.anderson.itsectorcodechallenge.di.Injectable
+import br.com.anderson.itsectorcodechallenge.extras.observe
+import br.com.anderson.itsectorcodechallenge.model.Photo
+import javax.inject.Inject
 
 class ListPhotoFragment : Fragment(R.layout.fragment_list_photo), Injectable {
 
+    lateinit var adapter: PhotoAdapter
+
+    val viewModel: ListPhotoViewModel by viewModels {
+        factory
+    }
+
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
+
+    private var fragmentBinding: FragmentListPhotoBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecycleView()
+        initrRefresh()
+        initRetryButton()
+        initScrollListener()
+        initObservers()
+        fetchPhotos()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentBinding = FragmentListPhotoBinding.inflate(layoutInflater)
+        viewModel.listPhotos()
+        
+    }
+
+    private fun initRetryButton() {
+        fragmentBinding?.retrybutton?.setOnClickListener(this::onRetryClick)
+    }
+
+    fun onRetryClick(view: View) {
+        viewModel.refresh()
+        view.isVisible = false
+    }
+
+    private fun initrRefresh() {
+        fragmentBinding?.swiperefresh?.setOnRefreshListener(this::onRefresh)
+    }
+
+    fun onRefresh() {
+        viewModel.refresh()
+    }
+
+    private fun fetchPhotos() {
+        viewModel.listPhotos()
+    }
+
+    private fun initObservers() {
+        observe(viewModel.dataPhoto, this::onLoadDataCompletedChallenge)
+        observe(viewModel.message, this::onMessage)
+        observe(viewModel.loading, this::onLoading)
+        observe(viewModel.clean, this::onClean)
+        observe(viewModel.retry, this::onRetry)
+    }
+
+    private fun initRecycleView() {
+        adapter = PhotoAdapter()
+        fragmentBinding?.recycleview?.adapter = adapter
+        fragmentBinding?.recycleview?.layoutManager = LinearLayoutManager(requireContext()).apply {
+            orientation = LinearLayoutManager.VERTICAL
+        }
+        adapter.itemOnClick = this::onItemClick
+    }
+
+    private fun onItemClick(item: Photo) {
+
+    }
+
+    private fun initScrollListener() {
+        val layoutManager = fragmentBinding?.recycleview?.layoutManager as LinearLayoutManager
+        fragmentBinding.recycleview.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val totalItemCount = layoutManager.itemCount
+                val visibleItemCount = layoutManager.childCount
+                val lastVisibleItem = layoutManager.findLastVisibleItemPosition()
+
+                viewModel.listScrolled(visibleItemCount, lastVisibleItem, totalItemCount)
+            }
+        })
+    }
+
+    private fun onLoadDataCompletedChallenge(data: List<Photo>) {
+        println("data - print onLoadDataCompletedChallenge")
+        adapter.submitList(data)
+    }
+
+    private fun onMessage(data: String) {
+        Toast.makeText(requireContext(), data, Toast.LENGTH_LONG).show()
+    }
+
+    private fun onLoading(data: Boolean) {
+        fragmentBinding?.progressloadingnextpage.isVisible = data && adapter.currentList.isNotEmpty()
+        fragmentBinding?.swiperefresh?.isRefreshing = data && adapter.currentList.isEmpty()
+    }
+
+    private fun onClean(data: Boolean) {
+        if (data)
+            adapter.submitList(arrayListOf())
+    }
+
+    private fun onRetry(data: String) {
+        Toast.makeText(requireContext(), data, Toast.LENGTH_LONG).show()
+        fragmentBinding?.retrybutton?.isVisible = true
+    }
+
+    override fun onDestroyView() {
+        // Consider not storing the binding instance in a field, if not needed.
+        fragmentBinding = null
+        super.onDestroyView()
+
     }
 }
---
+++ app/src/main/java/br/com/anderson/itsectorcodechallenge/ui/listphoto/PhotoAdapter.kt
@@ -0,0 +1,62 @@
+package br.com.anderson.itsectorcodechallenge.ui.listphoto
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.anderson.itsectorcodechallenge.R
+import br.com.anderson.itsectorcodechallenge.model.Photo
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import java.util.concurrent.Executors
+
+class PhotoAdapter : ListAdapter<Photo, PhotoAdapter.Holder>(
+    AsyncDifferConfig.Builder<Photo>(diffCallback)
+        .setBackgroundThreadExecutor(Executors.newSingleThreadExecutor())
+        .build()
+) {
+
+    var itemOnClick: (Photo) -> Unit = { _ -> }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoAdapter.Holder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_photo, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        binds(holder, getItem(position))
+    }
+
+    private fun binds(holder: Holder, data: Photo) {
+        with(holder) {
+            itemView.setOnClickListener {
+                itemOnClick(data)
+            }
+
+            Glide.with(image.context).load(data.smallUrl).apply(
+                    RequestOptions().centerCrop()).into(image)
+        }
+    }
+
+    inner class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView = view.findViewById(R.id.image)
+    }
+
+    companion object {
+        private val diffCallback: DiffUtil.ItemCallback<Photo> =
+            object : DiffUtil.ItemCallback<Photo>() {
+                override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+                    return oldItem == newItem
+                }
+
+                override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+                    return oldItem == newItem
+                }
+            }
+    }
+}
